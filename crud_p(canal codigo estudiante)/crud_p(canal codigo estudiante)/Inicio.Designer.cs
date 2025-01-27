8788382c152fbc259e703d006ef9fc03e6895b02
tony0001234 <132118721+tony0001234@users.noreply.github.com>
2025-01-27 17:46:18 -0600

    Add files via upload

---
+++ crud_p(canal codigo estudiante)/crud_p(canal codigo estudiante)/Inicio.Designer.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace crud_p_canal_codigo_estudiante_
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuTitulo = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPrueba = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // menuTitulo
+            // 
+            this.menuTitulo.AutoSize = false;
+            this.menuTitulo.BackColor = System.Drawing.Color.SteelBlue;
+            this.menuTitulo.Location = new System.Drawing.Point(0, 0);
+            this.menuTitulo.Name = "menuTitulo";
+            this.menuTitulo.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menuTitulo.Size = new System.Drawing.Size(532, 70);
+            this.menuTitulo.TabIndex = 1;
+            this.menuTitulo.Text = "menuStrip2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sistema de vacunacion";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(136, 281);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(193, 45);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtUsuario.Location = new System.Drawing.Point(195, 119);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtUsuario.Size = new System.Drawing.Size(292, 20);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(195, 158);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(292, 20);
+            this.txtPass.TabIndex = 5;
+            this.txtPass.TextChanged += new System.EventHandler(this.txtPass_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "USUARIO:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "CONTRASEÑA:";
+            // 
+            // txtPrueba
+            // 
+            this.txtPrueba.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtPrueba.Location = new System.Drawing.Point(30, 196);
+            this.txtPrueba.Multiline = true;
+            this.txtPrueba.Name = "txtPrueba";
+            this.txtPrueba.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtPrueba.Size = new System.Drawing.Size(457, 64);
+            this.txtPrueba.TabIndex = 8;
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 373);
+            this.Controls.Add(this.txtPrueba);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuTitulo);
+            this.Name = "Inicio";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuTitulo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPrueba;
+    }
+}
+
---
+++ crud_p(canal codigo estudiante)/crud_p(canal codigo estudiante)/Inicio.cs
@@ -0,0 +1,62 @@
+﻿using crud_p_canal_codigo_estudiante_.DAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace crud_p_canal_codigo_estudiante_
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            clsConexion.Conectar();
+            string consulta = "SELECT * FROM USUARIO WHERE USUARIO='"+txtUsuario.Text+"' AND CONTRASEÑA='"+txtPass+"' ";
+            SqlCommand comando = new SqlCommand(consulta, clsConexion.Conectar());
+            DataTable dt = new DataTable();
+            if (txtUsuario.Text == "" && txtPass.Text == "" || txtUsuario.Text == "" || txtPass.Text == "")
+            {
+                MessageBox.Show("Porfavor escriba su nombre y usuario");
+            }
+            else
+            {
+                try
+                {
+                    SqlDataReader lector = comando.ExecuteReader();
+
+                    if (lector.HasRows == true)
+                    {
+                        MessageBox.Show("Bienvenido");
+                        txtPrueba.Text =lector.GetString(1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o contraseña invalidos porfavor intente nuevamente");
+                    }
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Error: " + ex.ToString());
+                }
+            }
+            clsConexion.Close();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            clsConexion.Conectar();
+            MessageBox.Show("Conexion con base de datos exitosa");
+        }
+    }
+}
