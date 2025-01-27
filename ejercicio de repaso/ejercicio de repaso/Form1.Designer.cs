e7a5d8fad3f0f6d9af0fc4c2b65f938e2e20fd98
tony0001234 <132118721+tony0001234@users.noreply.github.com>
2025-01-27 17:47:06 -0600

    Add files via upload

---
+++ ejercicio de repaso/ejercicio de repaso/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ejercicio_de_repaso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txNombre = new System.Windows.Forms.TextBox();
+            this.txCodigo = new System.Windows.Forms.TextBox();
+            this.txSueldo = new System.Windows.Forms.TextBox();
+            this.txVer = new System.Windows.Forms.TextBox();
+            this.btnVer = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Codigo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sueldo:";
+            // 
+            // txNombre
+            // 
+            this.txNombre.Location = new System.Drawing.Point(94, 27);
+            this.txNombre.Name = "txNombre";
+            this.txNombre.Size = new System.Drawing.Size(470, 20);
+            this.txNombre.TabIndex = 3;
+            // 
+            // txCodigo
+            // 
+            this.txCodigo.Location = new System.Drawing.Point(94, 54);
+            this.txCodigo.Name = "txCodigo";
+            this.txCodigo.Size = new System.Drawing.Size(470, 20);
+            this.txCodigo.TabIndex = 4;
+            // 
+            // txSueldo
+            // 
+            this.txSueldo.Location = new System.Drawing.Point(94, 80);
+            this.txSueldo.Name = "txSueldo";
+            this.txSueldo.Size = new System.Drawing.Size(470, 20);
+            this.txSueldo.TabIndex = 5;
+            // 
+            // txVer
+            // 
+            this.txVer.Location = new System.Drawing.Point(62, 257);
+            this.txVer.Multiline = true;
+            this.txVer.Name = "txVer";
+            this.txVer.Size = new System.Drawing.Size(476, 240);
+            this.txVer.TabIndex = 6;
+            // 
+            // btnVer
+            // 
+            this.btnVer.Location = new System.Drawing.Point(15, 166);
+            this.btnVer.Name = "btnVer";
+            this.btnVer.Size = new System.Drawing.Size(75, 23);
+            this.btnVer.TabIndex = 7;
+            this.btnVer.Text = "ver";
+            this.btnVer.UseVisualStyleBackColor = true;
+            this.btnVer.Click += new System.EventHandler(this.btnVer_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(114, 166);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(215, 166);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 9;
+            this.btnEditar.Text = "editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(334, 166);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 10;
+            this.btnEliminar.Text = "eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 534);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnVer);
+            this.Controls.Add(this.txVer);
+            this.Controls.Add(this.txSueldo);
+            this.Controls.Add(this.txCodigo);
+            this.Controls.Add(this.txNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txNombre;
+        private System.Windows.Forms.TextBox txCodigo;
+        private System.Windows.Forms.TextBox txSueldo;
+        private System.Windows.Forms.TextBox txVer;
+        private System.Windows.Forms.Button btnVer;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+    }
+}
+
---
+++ ejercicio de repaso/ejercicio de repaso/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio_de_repaso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string nArchivo = "nArchivo.txt";
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombre = txNombre.Text.Trim();
+            string codigo = txCodigo.Text.Trim();
+            double sueldo;
+            if (string.IsNullOrEmpty(nombre) || string.IsNullOrEmpty(codigo) || !double.TryParse(txSueldo.Text, out sueldo))
+            {
+                MessageBox.Show("porfavor ingrese datos validos");
+                return;
+            }
+            using (StreamWriter sw = new StreamWriter(nArchivo, true))
+            {
+                sw.WriteLine($"{nombre}, {codigo}, {sueldo}");
+            }
+            limpiarCampos();
+        }
+        public void limpiarCampos()
+        {
+            txCodigo.Clear();
+            txSueldo.Clear();
+            txNombre.Clear();
+        }
+
+        private void btnVer_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(nArchivo))
+            {
+                string copiaTemp = File.ReadAllText(nArchivo);
+                txVer.Text = copiaTemp;
+            }
+            limpiarCampos ();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            string codigo = txCodigo.Text;
+            if(string.IsNullOrEmpty(codigo))
+            {
+                MessageBox.Show("porfavor ingrese un codigo para borrar su registro");
+                return;
+            }
+            if(!File.Exists(nArchivo))
+            {
+                MessageBox.Show("El archivo aun no ha sido creado");
+                return;
+            }
+            string[] copiaTemp = File.ReadAllLines(nArchivo);
+
+            using(StreamWriter sw =  new StreamWriter(nArchivo))
+            {
+                foreach(string s in copiaTemp)
+                {
+                    if (!s.Contains(codigo))
+                    {
+                        sw.WriteLine(s);
+                    }
+                }
+            }
+            limpiarCampos();
+            txVer.Clear();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            string nuevoNombre = txNombre.Text.Trim();
+            string codigo = txCodigo.Text.Trim();
+            double nuevoSueldo;
+            if (!File.Exists(nArchivo))
+            {
+                MessageBox.Show("El archivo aun no ha sido creado");
+                return;
+            }
+            if (string.IsNullOrEmpty(nuevoNombre) || string.IsNullOrEmpty(codigo) || !double.TryParse(txSueldo.Text, out nuevoSueldo))
+            {
+                MessageBox.Show("porfavor ingrese datos validos");
+                return;
+            }
+            string[] copiaTemp = File.ReadAllLines(nArchivo);
+
+            for (int i = 0; i < copiaTemp.Length; i++)
+            {
+                if (copiaTemp[i].Contains(codigo))
+                {
+                     copiaTemp[i] = $"{nuevoNombre}, {codigo}, {nuevoSueldo}";
+                     break;
+                }
+            }
+            File.WriteAllLines(nArchivo, copiaTemp);
+            MessageBox.Show("Edicion realizada con exito");
+            limpiarCampos();
+        }
+    }
+}
